5c528655709ad95d6932eb3c2b9fcf18655f8ea8
Piotr Kowalski <pkowalski@localhost.localdomain>
2014-11-05 02:11:08 +0000

    [F] Allow changing time on analog clock by rotating minute hand on touch

--- src/com/example/clock/MainActivity.java
+++ src/com/example/clock/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity implements OnTouchListener {
 
 	MyAnalogClock clock;
 
-	float x, y;
+	float eventX, eventY;
 
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,7 +32,7 @@ public class MainActivity extends Activity implements OnTouchListener {
 		clock.resume();
 		clock.setOnTouchListener(this);
 
-		x = y = 0;
+		eventX = eventY = 0;
 
 		setContentView(clock);
 	}
@@ -53,9 +53,11 @@ public class MainActivity extends Activity implements OnTouchListener {
 
 	public boolean onTouch(View v, MotionEvent event) {
 
-		x = event.getX();
-		y = event.getY();
-		
+		if (event.getAction() == MotionEvent.ACTION_MOVE) {
+			eventX = event.getX();
+			eventY = event.getY();
+		}
+
 		return true;
 	}
 
@@ -75,6 +77,13 @@ public class MainActivity extends Activity implements OnTouchListener {
 				clockTop, minuteHeight, minuteWidth, minuteLeft, minuteTop,
 				hourHeight, hourWidth, hourLeft, hourTop, minuteCenterX,
 				minuteCenterY, minuteDegrees, hourDegrees;
+
+		int minsHelper = 0;
+		int hourHelper = 0;
+
+		int q1, q2, q3, q4;
+
+		String hourFormat, minsFormat;
 
 		public MyAnalogClock(Context context) {
 
@@ -165,32 +174,127 @@ public class MainActivity extends Activity implements OnTouchListener {
 				hourMatrix.reset();
 				hourMatrix.postTranslate(minuteLeft, minuteTop);
 
-				if (x == 0 && y == 0) {
+				if (eventX == 0 && eventY == 0) {
 
 					minuteDegrees = hourDegrees = 0;
-				}
 
-				if (x != 0 && y != 0) {
+				} else {
 
-					minuteDegrees = ((int) Math.toDegrees(Math.atan2(minuteCenterY
-							- y, minuteCenterX - x))) - 90;
-					
-					if (minuteDegrees == -269) {
-						minuteDegrees = 360 + minuteDegrees;
+					minuteDegrees = ((int) Math.toDegrees(Math.atan2(
+							minuteCenterY - eventY, minuteCenterX - eventX))) - 90;
+
+					minsHelper = minuteDegrees;
+
+					if (minuteDegrees < 0) {
+						minsHelper = minuteDegrees + 360;
+					}
+
+					// 1st quarter
+					if (minuteDegrees >= 0 && minuteDegrees < 90) {
+						q1 = 1;
+						if (minuteDegrees > 0) {
+							q2 = q3 = 0;
+						}
+					}
+					// 2nd quarter
+					if (minuteDegrees >= -270 && minuteDegrees < -180) {
+						q2 = 1;
+						if (minuteDegrees > -270) {
+							q1 = q3 = q4 = 0;
+						}
+					}
+					// 3rd quarter
+					if (minuteDegrees >= -180 && minuteDegrees < -90) {
+						q3 = 1;
+						if (minuteDegrees > -180) {
+							q1 = q2 = q4 = 0;
+						}
+					}
+					// 4th quarter
+					if (minuteDegrees >= -90 && minuteDegrees < 0) {
+						q4 = 1;
+						if (minuteDegrees > -90) {
+							q2 = q3 = 0;
+						}
 					}
 					
-					hourDegrees = minuteDegrees / 12;
+					// Clockwise move
+					if (q4 == 1) {
+						if (minuteDegrees > -1) {
+							if (hourHelper < 24) {
+								q4 = 0;
+								hourHelper++;
+							} else {
+								hourHelper = 0;
+							}
+						}
+					}
+					
+					// Anti-clockwise move
+					if (q1 == 1) {
+						if (minuteDegrees < 0) {
+							if (hourHelper > 0) {
+								q4 = 1;
+								if (minuteDegrees < 0) {
+									q1 = 0;
+								}
+								hourHelper--;
+							} else {
+								hourHelper = 24;
+							}
+						}
+					}
+					
+					// What's the hour?
+					hourDegrees = (minsHelper + hourHelper * 360) / 12;
 				}
 
-				minuteMatrix.postRotate(minuteDegrees, minuteCenterX, minuteCenterY);
-				hourMatrix.postRotate(hourDegrees, minuteCenterX, minuteCenterY);
+				minuteMatrix.postRotate(minuteDegrees, minuteCenterX,
+						minuteCenterY);
+				hourMatrix
+						.postRotate(hourDegrees, minuteCenterX, minuteCenterY);
 
 				canvas.drawBitmap(hour, hourMatrix, null);
 				canvas.drawBitmap(minute, minuteMatrix, null);
 
 				paint.setColor(Color.BLACK);
 				paint.setTextSize(20);
-				canvas.drawText("" + minuteDegrees, 10, 25, paint);
+
+				canvas.drawText("eventX: " + (int) eventX, 10, 30, paint);
+				canvas.drawText("eventY: " + (int) eventY, 10, 60, paint);
+
+				canvas.drawText("mins degrees: " + minuteDegrees, 10, 100, paint);
+				canvas.drawText("hour degrees: " + hourDegrees, 10, 130, paint);
+
+				canvas.drawText("minsHelper: " + minsHelper, 10, 170, paint);
+				canvas.drawText("hourHelper: " + hourHelper, 10, 200, paint);
+				
+				canvas.drawText("q1: " + q1, 10, 240, paint);
+				canvas.drawText("q2: " + q2, 10, 270, paint);
+				canvas.drawText("q3: " + q3, 10, 300, paint);
+				canvas.drawText("q4: " + q4, 10, 330, paint);
+
+				// Format digital hours
+				if (hourHelper < 10) {
+					hourFormat = "0" + hourHelper;
+				} else {
+					if (hourHelper == 24) {
+						hourFormat = "00";
+					} else {
+						hourFormat = "" + hourHelper;
+					}
+				}
+				
+				// Format digital minutes
+				minsFormat = "" + minsHelper / 6;
+				if (minsHelper / 6 < 10) {
+					minsFormat = "0" + minsHelper / 6;
+				}
+				
+				// Display digital clock
+				paint.setColor(Color.RED);
+				paint.setTextSize(30);
+				canvas.drawText(hourFormat + ":" + minsFormat, 10, 370, paint);
 
 				holder.unlockCanvasAndPost(canvas);
 			}
